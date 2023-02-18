477af02f1ad01037988ff764ba16f2f61e091eeb
Md Alam Hossain <alam.rpi@hotmail.com>
2023-02-19 00:13:31 +0600

    Singleton Design pattern

---
+++ design-patterns/SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,42 @@
+﻿namespace design_patterns.SingletonDesignPattern
+{
+    /// <summary>
+    /// Only for Unit Test
+    /// </summary>
+    public interface ISingleton
+    {
+        string DoSomething();
+    }
+
+    public sealed class Singleton : ISingleton
+    {
+        private static Singleton? _instance;
+        public static object _obj;
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            //lock(_obj)
+            //{
+            //    if (_instance == null)
+            //        _instance = new Singleton();   
+            //}
+
+        //Latest version
+            lock (_obj)
+                _instance ??= new Singleton();
+            
+            return _instance;
+        }
+
+        /// <summary>
+        /// This method write for test
+        /// </summary>
+        /// <returns></returns>
+        public string DoSomething()
+        {
+            return "Test";
+        }
+    }
+}
---
+++ design-patterns/SingletonDesignPattern/SingletonTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using Xunit;
+
+namespace design_patterns.SingletonDesignPattern
+{
+    public class SingletonTests
+    {
+        [Fact]
+        public void TestDoSomethingMethod()
+        {
+            //Arrange
+            var mockSingleton = new Mock<ISingleton>();
+            mockSingleton.Setup(s => s.DoSomething()).Returns("Test");
+
+            //Act
+            string result = mockSingleton.Object.DoSomething();
+
+            // assert the result
+            Assert.Equal("Test", result);
+        }
+    }
+}
