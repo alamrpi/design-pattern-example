b9803dc80c55c8f3b8ac7ef8d4d140c10ba9bb07
Md Alam Hossain <alam.rpi@hotmail.com>
2023-02-19 07:51:05 +0600

    Factory Design Pattern

---
+++ design-patterns/FactoryDesignPattern/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.FactoryDesignPattern
+{
+    public interface IProduct
+    {
+        string GetName();
+    }
+
+    public class ProductA : IProduct
+    {
+        public string GetName()
+        {
+            return "Product A";
+        }
+    }
+
+    public class ProductB : IProduct
+    {
+        public string GetName()
+        {
+            return "Product B";
+        }
+    }
+
+}
---
+++ design-patterns/FactoryDesignPattern/ProductFactory.cs
@@ -0,0 +1,19 @@
+﻿
+namespace design_patterns.FactoryDesignPattern
+{
+    public class ProductFactory
+    {
+        public IProduct GetInstance(string productType)
+        {
+            switch (productType)
+            {
+                case "A":
+                    return new ProductA();
+                case "B":
+                    return new ProductB();
+                default:
+                    throw new ArgumentException("Invalid product type");
+            }
+        }
+    }
+}
